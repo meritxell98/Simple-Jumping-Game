6a692ba05b9c21fba637ea54a4ae08f66b236a42
Meritxell Jordan <meritxell98@hotmail.com>
2026-07-03 19:01:27 +0200

    get data and create character

---
+++ script.js
@@ -0,0 +1,10 @@
+const canvas = document.getElementById("game");
+const ctx = canvas.getContext("2d");
+
+const character = {
+    x: 50,
+    y: 230,
+    width: 20,
+    height: 50,
+    color: "red",
+}
